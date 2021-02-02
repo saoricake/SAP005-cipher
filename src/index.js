a8940c662b750c12ec248861001e582602e296e2
saori m <trhvmn.abcn@gmail.com>
2021-02-02 14:38:32 -0300

    Standardize event listener and code/decode functions

--- src/index.js
+++ src/index.js
@@ -6,9 +6,20 @@ let offsetNum = document.querySelector("#offsetNum");
 let encodeBtn = document.querySelector("#encodeBtn");
 let decodeBtn = document.querySelector("#decodeBtn");
 
-encodeBtn.addEventListener("click", function(){
-	ncodedTxt.value = cipher.encode(Number(offsetNum.value), dcodedTxt.value)
-});
-decodeBtn.addEventListener("click", function(){
-	dcodedTxt.value = cipher.decode(Number(offsetNum.value), ncodedTxt.value)
-});
+function listener(event) {
+	if (event.target == encodeBtn) {
+		var input = dcodedTxt;
+		var offset = Number(offsetNum.value);
+		var output = ncodedTxt;
+		var func = cipher.encode;
+	} else if (event.target == decodeBtn) {
+		var input = ncodedTxt;
+		var offset = -Number(offsetNum.value);
+		var output = dcodedTxt;
+		var func = cipher.decode;
+	}
+
+	output.value = func(offset, input.value);
+}
+
+[encodeBtn, decodeBtn].forEach(function(ele) {ele.addEventListener("click", listener);})
